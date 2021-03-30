fd3d14623eaa0ab6c8ef3f4cc04a0ce2761d1d8a
unknown <martin_suarez_mendez@outlook.com>
2021-03-30 15:08:56 -0600

    factories done 50 records on db

--- database/migrations/2021_03_30_172803_create_cursos_table.php
+++ database/migrations/2021_03_30_172803_create_cursos_table.php
@@ -15,6 +15,9 @@ class CreateCursosTable extends Migration
     {
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('categoria');
             $table->timestamps();
         });
     }
